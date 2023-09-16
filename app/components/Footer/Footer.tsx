f2eedb358a647dd000761a16f4d617a79c76c85e
ximo <ximo.gil@widitrade.com>
2023-09-16 08:56:56 +0200

    intial commit and first code implementation <3

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+export const Footer = () => {
+	return (
+		<footer className='px-5 py-5 border border-t border-gray-200 border-r-0 border-l-0 dark:border-0'>
+			<div className='flex justify-center items-center'>
+				<span>
+					Made with ❤️ ·{' '}
+					<a
+						href='https://github.com/ximo14/ximo'
+						target='_blank'
+						rel='noreferrer'
+						className='font-mono underline cursor-pointer'>
+						source
+					</a>
+				</span>
+			</div>
+		</footer>
+	)
+}
